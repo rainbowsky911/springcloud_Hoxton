da56c188f45d8226ec2c026b74b8747b3f864446
weishihuai <775334252@qq.com>
2020-08-26 20:39:59 +0800

    Spring Cloud Config 手动刷新配置文件

--- springcloud-config-client3355/src/main/java/com/wsh/springcloud/controller/TestController.java
+++ springcloud-config-client3355/src/main/java/com/wsh/springcloud/controller/TestController.java
@@ -3,10 +3,13 @@ package com.wsh.springcloud.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@RefreshScope注解的作用: 如果刷新了bean，那么下一次访问bean(即执行一个方法)时就会创建一个新实例
+@RefreshScope
 public class TestController {
     private static Logger logger = LoggerFactory.getLogger(TestController.class);
 
