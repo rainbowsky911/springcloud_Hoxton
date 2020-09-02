3a23de3023a6733aef8d6a75a5207aa8f01c7075
weishihuai <775334252@qq.com>
2020-09-02 21:54:29 +0800

    Spring Cloud Alibaba Sentinel流控

--- springcloudalibaba-sentinel-service8401/src/main/java/com/wsh/springcloud/alibaba/controller/SentinelController.java
+++ springcloudalibaba-sentinel-service8401/src/main/java/com/wsh/springcloud/alibaba/controller/SentinelController.java
@@ -3,11 +3,23 @@ package com.wsh.springcloud.alibaba.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class SentinelController {
 
     @GetMapping("/sentinel")
     public String sentinel() {
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(800);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        return "hello, sentinel dashboard....";
+    }
+
+    @GetMapping("/sentinel2")
+    public String sentinel2() {
         return "hello, sentinel dashboard....";
     }
 
