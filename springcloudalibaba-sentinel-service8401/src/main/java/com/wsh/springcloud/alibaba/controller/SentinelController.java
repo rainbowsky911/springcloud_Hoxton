a68662052f732c40084ee04c375038c70af6754c
weishihuai <775334252@qq.com>
2020-09-06 09:57:36 +0800

    Spring Cloud Alibaba Sentinel热点限流规则

--- springcloudalibaba-sentinel-service8401/src/main/java/com/wsh/springcloud/alibaba/controller/SentinelController.java
+++ springcloudalibaba-sentinel-service8401/src/main/java/com/wsh/springcloud/alibaba/controller/SentinelController.java
@@ -1,10 +1,13 @@
 package com.wsh.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.wsh.springcloud.alibaba.service.SentinelService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -74,5 +77,22 @@ public class SentinelController {
         return "testExceptionRate..";
     }
 
+    /**
+     * @SentinelResource: 指定资源名称，作用与HystrixCommand大体一致
+     * blockHandler: 指定降级处理方法，类似于Hystrix的fallbackMethod兜底方法
+     */
+    @GetMapping("/testHotKeyRule")
+    @SentinelResource(value = "testHotKeyRule", blockHandler = "testHotKeyRule_blockHandler")
+    public String testHotKeyRule(@RequestParam(value = "username", required = false) String username,
+                                 @RequestParam(value = "password", required = false) String password) {
+//        int i = 10 / 0;
+        return "[热点限流规则]testHotKeyRule..";
+    }
+
+    public String testHotKeyRule_blockHandler(@RequestParam(value = "username", required = false) String username,
+                                              @RequestParam(value = "password", required = false) String password,
+                                              BlockException exception) {
+        return "[热点限流规则-兜底方法]testHotKeyRule_blockHandler..";
+    }
 
 }
